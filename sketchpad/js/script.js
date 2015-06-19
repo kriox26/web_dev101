6716a8ef4bf04a2ec9b8b0b0eb6f69f821235307
kriox26 <matias.pan26@gmail.com>
2015-06-19 00:11:18 -0300

    correct indentation and trailling whitespace

--- sketchpad/js/script.js
+++ sketchpad/js/script.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
 	//Animations for the buttons
-		$("#black").mouseover(function(){
-			$("#black").fadeTo('fast',1);
-		});
-		$("#tilde").mouseover(function(){
-			$("#tilde").fadeTo('fast',1);
-		});
-		$("#color").mouseover(function(){
-			$("#color").fadeTo('fast',1);
-		});
-		$("#grey").mouseover(function(){
-			$("#grey").fadeTo('fast',1);
-		});
-		$("#clear").mouseover(function(){
-			$("#clear").fadeTo('fast',1);
-		});
-		$("#black").mouseleave(function(){
-			$("#black").fadeTo('fast',0.75);
-		});
-		$("#tilde").mouseleave(function(){
-			$("#tilde").fadeTo('fast',0.75);
-		});
-		$("#grey").mouseleave(function(){
-			$("#grey").fadeTo('fast',0.75);
-		});
-		$("#color").mouseleave(function(){
-			$("#color").fadeTo('fast',0.75);
-		});
-		$("#clear").mouseleave(function(){
-			$("#clear").fadeTo('fast',0.75);
-		});
-		buildGrid(16);
-		defaultPaint();
+	$("#black").mouseover(function(){
+		$("#black").fadeTo('fast',1);
+	});
+	$("#tilde").mouseover(function(){
+		$("#tilde").fadeTo('fast',1);
+	});
+	$("#color").mouseover(function(){
+		$("#color").fadeTo('fast',1);
+	});
+	$("#grey").mouseover(function(){
+		$("#grey").fadeTo('fast',1);
+	});
+	$("#clear").mouseover(function(){
+		$("#clear").fadeTo('fast',1);
+	});
+	$("#black").mouseleave(function(){
+		$("#black").fadeTo('fast',0.75);
+	});
+	$("#tilde").mouseleave(function(){
+		$("#tilde").fadeTo('fast',0.75);
+	});
+	$("#grey").mouseleave(function(){
+		$("#grey").fadeTo('fast',0.75);
+	});
+	$("#color").mouseleave(function(){
+		$("#color").fadeTo('fast',0.75);
+	});
+	$("#clear").mouseleave(function(){
+		$("#clear").fadeTo('fast',0.75);
+	});
+	buildGrid(16);
+	defaultPaint();
 });
 
 function numberSquares(){
@@ -48,7 +48,7 @@ function buildGrid(size){
 	for (var i = 0; i < (size*size); i++) {
 		$(".wrapper").append('<div class="square"></div>');
 	};
-$(".square").css("width",box);
+	$(".square").css("width",box);
 	$(".square").css("height",box);
 }
 
@@ -59,7 +59,7 @@ function defaultPaint(){
 }
 
 function clearGrid(){
-	x = numberSquares(); 
+	x = numberSquares();
 	buildGrid(x);
 	defaultPaint();
 }
